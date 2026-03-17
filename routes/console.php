44b616360d71e181815138cfc563ec100fd7941d
Febri Nahrul Kha <fn.febriii@gmail.com>
2026-03-17 09:12:16 +0700

    Jobs Reset Cuti Saat Anniv Karywan
    [+] Command Reset Cuti
    [+] Jobs Reset Cuti
    [+] Console Schedule Reset Daily 00:30
    [+] Setup database for queue

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// jalankan reset cuti setiap hari jam 00.01
+Schedule::command('cuti:reset')
+    ->dailyAt('00:30')
+    ->withoutOverlapping() //cegah overlap jika job sebelumnya masih berjalan
+    ->runInBackground() //jalankan di background
+    ->appendOutputTo(storage_path('logs/cuti-reset.log'))
+    ->description('Reset cuti tahunan berdasarkan tgl masuk karyawan');
