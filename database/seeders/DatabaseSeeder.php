a1ce6d5b39e71f50fb7b8f981223e50ab3062491
Dimas Faqih <dimasfaqih005@gmail.com>
2026-07-30 08:46:19 +0700

    chore(gitignore): sync dummy seeder ignore rules, DatabaseSeeder, and MenuSeeder updates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,13 +11,53 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
+        // Core / Master Seeders (Always executed)
+        $mainSeeders = [
             PermissionSeeder::class,
             RoleSeeder::class,
             UserSeeder::class,
             MenuSeeder::class,
             PerusahaanSeeder::class,
             WilayahSeeder::class,
-        ]);
+            UmDataSeeder::class,
+            UmumSeeder::class,
+            PayrollSeeder::class,
+            SdmPayrollGolonganMatrixSeeder::class,
+            PayrollPph21ReferenceSeeder::class,
+            CutiBersamaSeeder::class,
+            StrukturOrganisasiSeeder::class,
+        ];
+
+        foreach ($mainSeeders as $seeder) {
+            if (class_exists($seeder)) {
+                $this->call($seeder);
+            }
+        }
+
+        // Optional / Dummy Seeders (Only executed if present locally)
+        $optionalDummySeeders = [
+            'Database\Seeders\DokterSeeder',
+            'Database\Seeders\SuperAdminSignatureSeeder',
+            'Database\Seeders\DummyDataSeeder',
+            'Database\Seeders\JadwalSeeder',
+            'Database\Seeders\KaryawanExcelSeeder',
+            'Database\Seeders\DummyPayrollSlipSeeder',
+            'Database\Seeders\JadwalDummyJuniSeeder',
+            'Database\Seeders\SkenarioTriRahayuSeeder',
+            'Database\Seeders\JadwalDuaTahunSeeder',
+            'Database\Seeders\DummySdmSeeder',
+            'Database\Seeders\RuanganDummySeeder',
+            'Database\Seeders\JadwalAbsensiJuli2026Seeder',
+            'Database\Seeders\InpatientSeeder',
+            'Database\Seeders\DokterPoliSeeder',
+            'Database\Seeders\DokterAllPoliSeeder',
+        ];
+
+        foreach ($optionalDummySeeders as $seeder) {
+            if (class_exists($seeder)) {
+                $this->call($seeder);
+            }
+        }
     }
 }
+
