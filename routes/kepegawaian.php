d3bb98910c589e639f380e89d70cb29050250f05
Vian-98 <rizki.favian123@gmail.com>
2026-07-30 08:39:52 +0700

    chore(routes): add routes for Paket 2 - Absensi & Presensi

--- routes/kepegawaian.php
+++ routes/kepegawaian.php
@@ -11,17 +11,6 @@ Route::prefix('karyawan')
         Route::get('/edit/{id}', App\Livewire\Karyawan\Edit::class)->name('edit');
     });
 
-<<<<<<< HEAD
-=======
-// Jadwal Kerja
-Route::prefix('jadwal-kerja')
-    ->name('jadwal-kerja.')
-    ->group(function () {
-        Route::get('/', App\Livewire\Kepegawaian\JadwalKerja\Index::class)->name('index');
-        Route::get('/tukar-dokter', App\Livewire\Kepegawaian\JadwalKerja\TukarJadwal::class)->name('tukar-dokter');
-        Route::get('/kelola/{id}', App\Livewire\Kepegawaian\JadwalKerja\Kelola::class)->name('kelola');
-    });
-
 // Absensi
 Route::prefix('absensi')
     ->name('absensi.')
@@ -32,14 +21,6 @@ Route::prefix('absensi')
         Route::get('/rekonsiliasi/{batchId}', App\Livewire\Kepegawaian\Absensi\Rekonsiliasi::class)->name('rekonsiliasi');
         Route::get('/rekap', App\Livewire\Kepegawaian\Absensi\Rekap::class)->name('rekap');
     });
-
-// Konfigurasi Jadwal
-Route::prefix('konfigurasi-jadwal')
-    ->name('konfigurasi-jadwal.')
-    ->group(function () {
-        Route::get('/', App\Livewire\Kepegawaian\KonfigurasiJadwal::class)->name('index');
-    });
->>>>>>> 253fd8c (feat(absensi): add duplicate tap report page and csv export functionality)
 
 // Master data
 Route::prefix('master')
