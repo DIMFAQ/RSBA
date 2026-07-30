b9ebeed9d36e0a2661ea2eee73b18142ba05dc9c
Vian-98 <rizki.favian123@gmail.com>
2026-07-30 08:42:03 +0700

    chore(routes): add routes for Paket 3 - Cuti Bersama & Master SDM

--- routes/kepegawaian.php
+++ routes/kepegawaian.php
@@ -57,18 +57,15 @@ Route::prefix('surat')
     ->name('surat.')
     ->group(function () {
         Route::get('cuti', App\Livewire\Surat\Cuti\Index::class)->name('cuti');
-        // Route::get('cuti/approval/{id?}', App\Livewire\Surat\Cuti\Approval::class)->name('cuti.approval');
-
-<<<<<<< HEAD
         Route::get('sp3', App\Livewire\Surat\Sp3\Index::class)->name('sp3');
-=======
+    });
+
 // Cuti Bersama
 Route::prefix('cuti-bersama')
     ->name('cuti-bersama.')
     ->group(function () {
         Route::redirect('/', '/kepegawaian/surat/cuti?tab=cuti-bersama')->name('index');
         Route::get('/{id}', App\Livewire\Kepegawaian\CutiBersama\Show::class)->name('show');
->>>>>>> 9206642 (feat(ui): gabungkan Cuti Bersama ke dalam halaman Izin & Cuti dengan 2-Tab Layout)
     });
 
 
